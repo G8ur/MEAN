391e78a294fff18f0ad2a252ad12b62c6fb38d04
No Need <77963254+G8ur@users.noreply.github.com>
2023-06-15 10:14:55 +0530

    Images and all done

---
+++ frontend/src/data.ts
@@ -0,0 +1,73 @@
+import { Food } from "./app/shared/models/Food";
+
+export const sample_foods: Food[]= [
+  {
+    id:'1',
+    name:'Nike Pegasus',
+    price: 40,
+
+    // cookTime
+    favorite:false,
+    origins:['pennsylvinia','usa'],
+    stars: 4.0,
+    imageUrl: 'assets/nike1.jpeg',
+    tags: ['Shoes','Nike'],
+
+
+  },
+  {
+    id: '2',
+    name:'Nike AirJordan',
+    price: 40,
+    // cookTime
+    favorite:false,
+    origins:['pennsylvinia','usa'],
+    stars: 4.0,
+    imageUrl: 'assets/nike2.jpeg',
+    tags: ['Shoes','Nike'],
+  },
+  {
+    id: '3',
+    name:'Nike AirFlow',
+    price: 40,
+    // cookTime
+    favorite:false,
+    origins:['pennsylvinia','usa'],
+    stars: 4.0,
+    imageUrl: 'assets/nike3.jpeg',
+    tags: ['Shoes','Nike'],
+  },
+  {
+    id: '4',
+    name:'Nike AirJordan',
+    price: 40,
+    // cookTime
+    favorite:false,
+    origins:['pennsylvinia','usa'],
+    stars: 4.0,
+    imageUrl: 'assets/nike4.jpeg',
+    tags: ['Shoes','Nike'],
+  },
+  {
+    id: '5',
+    name:'Nike AirJordan',
+    price: 40,
+    // cookTime
+    favorite:true,
+    origins:['pennsylvinia','usa'],
+    stars: 4.0,
+    imageUrl: 'assets/nike5.jpeg',
+    tags: ['Shoes','Nike'],
+  },
+  {
+    id: '6',
+    name:'Nike AirJordan',
+    price: 40,
+    // cookTime
+    favorite:false,
+    origins:['pennsylvinia','usa'],
+    stars: 4.0,
+    imageUrl: 'assets/nike6.jpeg',
+    tags: ['Shoes','Nike'],
+  },
+]
