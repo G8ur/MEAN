d4b01f5ebb9c02fe8d3a47567ec7a34479205276
No Need <77963254+G8ur@users.noreply.github.com>
2023-06-16 22:07:12 +0530

    Basic

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { CartPageComponent } from './componets/pages/cart-page/cart-page.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'search/:searchTerm', component:HomeComponent},
   {path:'tag/:tag',component:HomeComponent},   // in tag/:tag tag is literal and /:tag is paramter
-  {path: 'food/:id',component:FoodPageComponent}
+  {path: 'food/:id',component:FoodPageComponent},
+  {path:'cart-page',component:CartPageComponent}
 ];  // here we can add our routes
 
 @NgModule({
